dbcc53bcf9b59873d3a6f9c3ef8f0f7077e1fd99
krishnareddygari25 <krishnareddygarianusha@gmail.com>
2023-11-20 22:47:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anusha(request):
+    return render(request,'anusha.html')
+def anil(request):
+    return render(request,'anil.html')
+def mom(request):
+    return render(request,'mom.html')
